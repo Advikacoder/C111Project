2ae9fd542905e64f82656d5b1b423eb329d5c9ac
Advikacoder <92722900+Advikacoder@users.noreply.github.com>
2022-01-03 14:47:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifer =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/toioXnQED/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = windowSynthesis;
+    speak_data_1 = "The first prediction is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
